090b692af6cecf31409faf0edd61e072c21a927d
Jisu Han <weblite@naver.com>
2022-06-23 23:58:07 +0900

    myBatis practice, Oracle11g,gradle 연결

--- src/main/java/study/mvc/MvcApplication.java
+++ src/main/java/study/mvc/MvcApplication.java
@@ -1,8 +1,10 @@
 package study.mvc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value={"study.mvc.mapper"})
 @SpringBootApplication
 public class MvcApplication {
 
